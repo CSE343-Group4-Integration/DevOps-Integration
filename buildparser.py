c30f5c4a09b5c67098cc1d3a07d54962f234aaf6
onurkucuk <onurkucukgg@gmail.com>
2017-11-24 17:22:23 +0300

    Create buildparser.py

---
+++ buildparser.py
@@ -0,0 +1,43 @@
+import  xml.etree.ElementTree as ET
+from xml.dom import minidom
+
+tree = ET.parse('empty_project.xml')
+e = tree.getroot()
+
+#Parametreler:
+#text = yazacagimiz string
+#eltree = xmli cektigimiz elementTree
+#tag = xml tagi
+def setter(text, eltree, tag):
+	for child in e:
+		if(child.tag == tag):
+			child.text = text
+			break
+
+#Parametreler:
+#eltree = xmli cektigimiz elementTree
+#tag = xml tagi
+def getter(eltree, tag):
+	for child in e:
+		if(child.tag == tag):
+			return child.text
+
+def getProjectName(eltree):
+	return getter(eltree, "projectName")
+	
+def getBuildResult(eltree):
+	return getter(eltree, "buildResult")
+	
+def getBuildResultDetail(eltree):
+	return getter(eltree, "buildResultDetail")
+	
+def getTargetUrl(eltree):
+	return getter(eltree, "targetUrl")
+
+def setBuildResult(text, eltree):
+	setter(text, eltree, "buildResult")
+
+def setBuildResultDetail(text, eltree):
+	setter(text, eltree, "buildResultDetail")
+
+tree.write('output.xml')
