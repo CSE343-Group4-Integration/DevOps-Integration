84907007925d491e723b9bce854baa97360ba104
Yavuz Selim <yserdol@gtu.edu.tr>
2017-11-27 10:12:10 +0300

    updating for performance

--- parser.py
+++ parser.py
@@ -7,17 +7,14 @@ def parse_xml(config_xml, tag):
     :param config_xml: must be a xml string
     :param tag: must be a  attribute
     :return:
-    Raise 3 Exception;
-        1. for config_file  exits or readable
-        2. for file may be empty or may not be the xml extension
-    """
-    try:
-        soup = BeautifulSoup(config_xml, 'xml')
+    Raise 1 Exception;
+        1. file may be empty or may not be the xml extension
 
-        attribute = soup.find(tag)
-        if attribute is None:
-            raise Exception('Cant find tag')
-        else:
-            return attribute.text
-    except IOError:
-        raise IOError("Can't find config file")
+    """
+    soup = BeautifulSoup(config_xml, 'xml')
+
+    attribute = soup.find(tag)
+    if attribute is None:
+        raise Exception("Can't find  " + tag)
+    else:
+        return attribute.text
