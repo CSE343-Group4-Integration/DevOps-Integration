51f0743c7bf3b89447bfa95964782285a5d081c2
onurkucuk <onurkucukgg@gmail.com>
2017-11-30 17:38:01 +0300

    Update jsontoxmlparser.py
    Fonksiyonlar genele çekildi, artık obje yerine string alıyor

--- jsontoxmlparser.py
+++ jsontoxmlparser.py
@@ -21,7 +21,7 @@ jsonString = '''{
         "publishers": null,
         "buildWrappers": null,
         "githubUrl": null,
-        "projectName": null,
+        "projectName": "asdasd",
         "commitId": null,
         "targetUrl": null,
         "buildResult": null,
@@ -30,51 +30,32 @@ jsonString = '''{
     }
 }'''
 
-#Parametreler:
-#text = yazacagimiz string
-#eltree = xmli cektigimiz elementTree
-#tag = xml tagi
-def setter(text, eltree, tag):
-	for child in e:
-		if(child.tag == tag):
-			child.text = text
-			break
-
-#Parametreler:
-#eltree = xmli cektigimiz elementTree
-#tag = xml tagi
-def getter(eltree, tag):
-	for child in e:
-		if(child.tag == tag):
-			return child.text
-
-def getProjectName(eltree):
-	return getter(eltree, "projectName")
-	
-def getBuildResult(eltree):
-	return getter(eltree, "buildResult")
-	
-def getBuildResultDetail(eltree):
-	return getter(eltree, "buildResultDetail")
-	
-def getTargetUrl(eltree):
-	return getter(eltree, "targetUrl")
-
-def setBuildResult(text, eltree):
-	setter(text, eltree, "buildResult")
-
-def setBuildResultDetail(text, eltree):
-	setter(text, eltree, "buildResultDetail")
-
 def jsontoxml(jsonString):
 	return xmltodict.unparse(json.loads(jsonString), pretty=True)
 	
 def xmltojson(xmlString):
 	return json.dumps(xmltodict.parse(EMPTY_CONFIG_XML), indent=4)
 
+#Parametreler:
+#text = yazacagimiz string
+#eltree = xmli cektigimiz elementTree
+#tag = xml tagi
+def setter(text, xmlString, tag):
+	e = ET.fromstring(xmlString)
+	for child in e:
+		if(child.tag == tag):
+			child.text = text
+			break
+	return ET.tostring(e)
+
+#Parametreler:
+#eltree = xmli cektigimiz elementTree
+#tag = xml tagi
+def getter(xmlString, tag):
+	e = ET.fromstring(xmlString)
+	for child in e:
+		if(child.tag == tag):
+			return child.text
+
 xmlString = jsontoxml(jsonString)
 
-e = ET.fromstring(xmlString)
-
-for child in e:
-	print(child.tag, " ", child.text)
