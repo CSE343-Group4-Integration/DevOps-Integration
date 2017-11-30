dc753f8d89405a67de4fd45c4337eff47af08929
bedo190306 <mbcaglar@gtu.edu.tr>
2017-11-30 23:59:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 This example shows how to add new command to "Shell" build step
 """
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 import xml.etree.ElementTree as ET
 import jenkins
 import json
 import xmltodict
+import requests
 import parserDeneme as parser
 EMPTY_CONFIG_XML = '''<?xml version='1.0' encoding='UTF-8'?>
 <project>
@@ -24,7 +25,7 @@ EMPTY_CONFIG_XML = '''<?xml version='1.0' encoding='UTF-8'?>
   <buildWrappers/>
   <methodname>createjob</methodname>
   <githubUrl></githubUrl><!--Code&plan-->
-  <projectName></projectName><!--Code&plan--><!--Build,Deployment-->
+  <projectName>deneme5</projectName><!--Code&plan--><!--Build,Deployment-->
   <commitId></commitId><!--Code&plan--><!--Deployment-->
   <targetUrl></targetUrl><!--Code&plan--><!--Build,Test-->
   <buildResult></buildResult><!--Build--><!--Code&plan(fail),Test(Pass)-->
@@ -41,15 +42,35 @@ def getter(xmlString, tag):
     if(child.tag == tag):
       return child.text
 
+def createJob(project_name):
+  server.create_job(project_name,EMPTY_CONFIG_XML)
+
+def deleteJob(project_name):
+  server.delete_job(project_name)
+
 
 def mainFunction(jsonfile):
 
     xmlfile=parser.Json2Xml(jsonfile)
     methodname=getter(xmlfile,'methodname')
     if methodname=="createjob":
-      print("create")
+      createJob(str(getter(EMPTY_CONFIG_XML, 'projectName')))
+      payload = {
+          "id": "1",
+          "name": "TestProject",
+          "owner": "Group7",
+          "method": "create"
+      }
+      #requests.post("http://localhost:8081/"+"Deployment", data=json.dumps(payload))
     elif methodname=="deletejob":
-      print("delete")
+      deleteJob(str(getter(EMPTY_CONFIG_XML, 'projectName')))
+      payload = {
+          "id": "1",
+          "name": "TestProject",
+          "owner": "Group7",
+          "method": "delete"
+      }
+      #requests.post("http://localhost:8081/"+"Deployment", data=json.dumps(payload))
     elif methodname=="build":
       print("build")
     elif methodname=="check-build-status":
@@ -59,6 +80,7 @@ def mainFunction(jsonfile):
     elif methodname=="check-deploy-status":
       print("create")
 
+server = jenkins.Jenkins('http://localhost:8080/', username='admin',password='1234')
 mainFunction(parser.xml2Json(EMPTY_CONFIG_XML))
 '''
 server = jenkins.Jenkins('http://localhost:8080',"mehmet","4444")
@@ -68,4 +90,3 @@ jsonlast=JsonToXml(XmlToJson(my_job))
 print(jsonlast)
 '''
  # prints XML configuration
-
