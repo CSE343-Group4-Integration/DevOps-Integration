e813c5407c04cb25bbbd79e699c92a160e8da12e
Lütfullah TÜRKER <lutfullahturker@gmail.com>
2017-11-30 22:12:58 +0300

    Get any projects Xml file and set any tag  in Xml

---
+++ GetAndSetXML.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import jenkins
+import jsontoxmlparser as parser
+
+'''
+create_job(name, config_xml)
+Create a new Jenkins job
+
+Parameters:	
+name – Name of Jenkins job, str
+config_xml – config file text, str
+
+/////////////////////////////////////////
+
+get_job_config(name)
+Get configuration of existing Jenkins job.
+
+Parameters:	name – Name of Jenkins job, str
+Returns:	job configuration (XML format)
+
+/////////////////////////////////////////
+
+reconfig_job(name, config_xml)
+Change configuration of existing Jenkins job.
+
+To create a new job, see Jenkins.create_job().
+
+Parameters:	
+name – Name of Jenkins job, str
+config_xml – New XML configuration, str
+
+/////////////////////////////////////////
+
+get_node_config(name)
+Get the configuration for a node.
+
+Parameters:	name – Jenkins node name, str
+
+/////////////////////////////////////////
+
+reconfig_node(name, config_xml)
+Change the configuration for an existing node.
+
+Parameters:	
+name – Jenkins node name, str
+config_xml – New XML configuration, str
+
+/////////////////////////////////////////
+'''
+
+
+server = jenkins.Jenkins('http://localhost:8080', username='lutfullah', password='Password')
+user = server.get_whoami()
+
+
+print ("////////////////////////////////////////////////////")
+print('Hello %s  ' % (user['fullName']))
+print ("////////////////////////////////////////////////////")
+
+
+def getConfigXML(projectName):
+    return server.get_job_config(projectName)
+
+def setConfigXML(projectName,xmlFile,tagName,newTagValue):
+    str = parser.setter(newTagValue,xmlFile,tagName)
+    server.reconfig_job(projectName, str)
+    return server.get_job_config(projectName)
+
+#server.create_job('testJob', jenkins.EMPTY_CONFIG_XML)
+
+print (getConfigXML("testJob"))
+print ("////////////////////////////////////////////////////")
+
+print (setConfigXML('testJob',getConfigXML("testJob"),'builders','Lutfullah TURKER'))
+
+
