087e4d826e8c9195ba64f19dd62054a3a7826303
onurkucuk <onurkucukgg@gmail.com>
2017-12-04 18:58:19 +0300

    Update createGeneralReq.py

--- createGeneralReq.py
+++ createGeneralReq.py
@@ -3,9 +3,11 @@ import jenkins
 import json_io as js
 import xml.etree.ElementTree as ET
 
+tags = ['object_type', 'github_login', 'github_password', 'card_id', 'repository_url', 'project_name', 'method']
+
 server = jenkins.Jenkins('http://localhost:8080', username='skole', password='1234123121')
 	
-def createGeneralReq(projectName):
+def creteGeneralReq(projectName):
 	generalReq = '''{  "$schema": "http://json-schema.org/draft-04/schema#",
        "title": "Request information",
        "type": "object",
@@ -23,7 +25,6 @@ def createGeneralReq(projectName):
 	configXML = server.get_job_config(projectName)
 	e = ET.fromstring(configXML)
 	for child in e:
-		generalReq = js.json_setter(generalReq, child.tag, child.text)
+		if(child.tag in tags):
+			generalReq = js.json_setter(generalReq, child.tag, child.text)
 	return generalReq
-
-print(createGeneralReq('denemeJson'))
