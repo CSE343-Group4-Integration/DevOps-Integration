ad980f5fb5a01ab974bffdfb2ac3f1f8063f7eb5
MEHMET ONDER <m.onder@gtu.edu.tr>
2017-12-04 18:01:19 +0300

    Update GetAndSetXML.py

--- GetAndSetXML.py
+++ GetAndSetXML.py
@@ -6,14 +6,14 @@ import jsontoxmlparser as parser
 server = jenkins.Jenkins('http://localhost:8080', username='skole', password='1234123121')
 
 def getConfigXML(projectName):
-    return server.get_job_config(projectName)
+	return server.get_job_config(projectName)
 
 def reConfig(projectName,tagName,newTagValue):
 	strconfig = server.get_job_config(projectName)
-    strconfig = parser.setter(newTagValue,strconfig,tagName)
-    server.reconfig_job(projectName, strconfig)
-    return server.get_job_config(projectName)
+	strconfig = parser.setter(newTagValue,strconfig,tagName)
+	server.reconfig_job(projectName, strconfig)
+	return server.get_job_config(projectName)
 
 def reConfigWithXml(projectName,xmlFile):
-    server.reconfig_job(projectName, xmlFile)
-    return server.get_job_config(projectName)
+	server.reconfig_job(projectName, xmlFile)
+	return server.get_job_config(projectName)
