9a20cc0d0c33013efd62d92be3ac9c0200f8d003
Rıdvan <rdemirci@gtu.edu.tr>
2017-12-07 21:11:53 +0200

    Update createGeneralReq.py
    interface'e göre guncellendi

--- createGeneralReq.py
+++ createGeneralReq.py
@@ -5,14 +5,10 @@ import xml.etree.ElementTree as ET
 
 tags = ['object_type', 'github_login', 'github_password', 'card_id', 'repository_url', 'project_name', 'method']
 
-server = jenkins.Jenkins('http://localhost:8080', username='skole', password='1234123121')
+server = jenkins.Jenkins('http://localhost:8080', username='mustafabingultr', password='kral')
 	
 def createGeneralReq(projectName):
-	generalReq = '''{  "$schema": "http://json-schema.org/draft-04/schema#",
-       "title": "Request information",
-       "type": "object",
-       "description": "Information necessary to access project sources on github repository and method to be applied",
-       "properties": {
+	generalReq = '''{  
          "object_type": {"type": "string"},
          "github_login": { "type": "string" },
          "github_password": { "type": "string" },
@@ -20,7 +16,7 @@ def createGeneralReq(projectName):
          "repository_url": { "type": "string" },
          "project_name": { "type": "string" },
          "method": { "type": "string" }
-       }
+       
 	}'''
 	configXML = server.get_job_config(projectName)
 	e = ET.fromstring(configXML)
