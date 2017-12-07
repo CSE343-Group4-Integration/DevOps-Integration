b55b0bef055e2153b0057fd702a581895b4f06b8
Rıdvan <rdemirci@gtu.edu.tr>
2017-12-07 21:12:41 +0200

    Update request.py
    interface'e gore guncellendi

--- request.py
+++ request.py
@@ -1,7 +1,5 @@
 #request islemini gerceklestiren method
 import  requests
-import json
 
 def postRequest(jsonData, group):
-	requests.post("http://localhost:8081/"+str(group), data=json.dumps(jsonData))
-
+	requests.post("http://localhost:8081/"+str(group), data=jsonData)
